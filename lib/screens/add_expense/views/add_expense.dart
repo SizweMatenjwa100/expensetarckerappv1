0ef5f3901fcef9b0b47656931af8b33b9a129d8f
Sizwe Matenjwa <SizweMatenjwa100@gmail.com>
2025-04-06 16:39:30 +0200

    updated the add expsense screen with color picker

--- lib/screens/add_expense/views/add_expense.dart
+++ lib/screens/add_expense/views/add_expense.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_colorpicker/flutter_colorpicker.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:intl/intl.dart';
 
@@ -14,6 +16,17 @@ class _AddExpenseState extends State<AddExpense> {
   TextEditingController categoryController= TextEditingController();
   TextEditingController dateController= TextEditingController();
   DateTime selectDate=DateTime.now();
+  List<String> myCategoriesIcon=[
+    'burger',
+    'entertainment',
+    'home',
+    'shopping',
+    'tech',
+    'travel'
+
+
+  ];
+late String iconSelected='';
 
   @override
   void initState(){
@@ -71,63 +84,155 @@ class _AddExpenseState extends State<AddExpense> {
                       onPressed: () {
                       showDialog(context: (context),
                           builder: (ctx){
-                        return AlertDialog(
+                        late bool isExpended=false;
+                        return StatefulBuilder(
+                          builder: (context,setState) {
+                            return AlertDialog(
 
-                          content: Column(
-                            mainAxisSize: MainAxisSize.min,
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Text("Create a Category", style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold, color: Colors.grey.shade600),),
-                              SizedBox(height: 12,),
+                              content: SizedBox(
+                                width: MediaQuery.of(context).size.width,
+                                child: Column(
+                                  mainAxisSize: MainAxisSize.min,
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Text("Create a Category", style: TextStyle(
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.grey.shade600),),
+                                    SizedBox(height: 12,),
 
-                              TextFormField(
+                                    TextFormField(
 
-                                textAlignVertical: TextAlignVertical.center,
-                                decoration: InputDecoration(
-                                    filled: true,
-                                    fillColor:Colors.white,
-                                    hintText: "Name",hintStyle: TextStyle(color: Colors.grey, fontWeight: FontWeight.bold),
-                                    border: OutlineInputBorder(
-                                        borderRadius: BorderRadius.circular(12),
-                                        borderSide: BorderSide.none
+                                      textAlignVertical: TextAlignVertical.center,
+                                      decoration: InputDecoration(
+                                          filled: true,
+                                          fillColor: Colors.white,
+                                          hintText: "Name",
+                                          hintStyle: TextStyle(color: Colors.grey,
+                                              fontWeight: FontWeight.bold),
+                                          border: OutlineInputBorder(
+                                              borderRadius: BorderRadius.circular(
+                                                  12),
+                                              borderSide: BorderSide.none
 
+                                          )
+                                      ),
+                                    ),
+                                    SizedBox(height: 16,),
+                                    TextFormField(
+                                      onTap: () {
+                                        setState(() {
+                                          isExpended = !isExpended;
+                                        });
+                                      },
+                                      readOnly: true,
+                                      textAlignVertical: TextAlignVertical.center,
+                                      decoration: InputDecoration(
+                                          suffixIcon: Icon(
+                                              CupertinoIcons.chevron_down,
+                                              size: 16),
+                                          filled: true,
+                                          fillColor: Colors.white,
+                                          hintText: "Icon",
+                                          hintStyle: TextStyle(color: Colors.grey,
+                                              fontWeight: FontWeight.bold),
+                                          border: OutlineInputBorder(
+                                              borderRadius:isExpended?
+                                              BorderRadius.vertical(
+                                                  top: Radius.circular(16)
+                                              )
+                                              : BorderRadius.circular(12),
+                                              borderSide: BorderSide.none
+
+                                          )
+                                      ),
+                                    ),
+                                    isExpended ?
+                                    Container(
+                                      width: MediaQuery.of(context).size.width,
+                                      height: 200,
+
+                                      decoration: BoxDecoration(
+                                          color: Colors.white,
+                                          borderRadius: BorderRadius.vertical(
+                                              bottom: Radius.circular(16)
+                                          )
+                                      ),
+                                      child: Padding(
+                                        padding: const EdgeInsets.all(8.0),
+                                        child: GridView.builder(
+                                            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                                              crossAxisCount:3,
+                                              mainAxisSpacing:5,
+                                              crossAxisSpacing:5,
+                                            ),
+                                            itemCount:myCategoriesIcon.length ,
+                                            itemBuilder:(context, int i){
+                                              return GestureDetector(
+                                                onTap: (){
+                                                  setState((){
+                                                    iconSelected = myCategoriesIcon[i];
+                                                  });
+                                                },
+                                                child: Container(
+                                                  width: 50,
+                                                  height: 50,
+                                                  decoration: BoxDecoration(
+
+                                                    border: Border.all(
+                                                      width: 2,
+                                                      color: iconSelected==myCategoriesIcon[i]? Colors.green: Colors.grey
+                                                    ),
+                                                    borderRadius: BorderRadius.circular(12),
+                                                    image: DecorationImage(image: AssetImage(
+                                                      'assets/${myCategoriesIcon[i]}.png'
+                                                    ),
+                                                    )
+                                                  ),
+                                                ),
+                                              );
+
+                                        }
+                                        ),
+                                      ),
                                     )
+                                        : Container(),
+                                    SizedBox(height: 16,),
+                                    TextFormField(
+                                      onTap: (){
+                                        showDialog(context: context, builder:(ctx2){
+                                          return AlertDialog(
+                                            content: ColorPicker(
+                                              pickerColor:Colors.blue ,
+                                              onColorChanged: (value) {
+                                            
+                                              },
+                                            ),
+                                          );
+                                        },
+                                        );
+                                      },
+                                      readOnly: true,
+                                      textAlignVertical: TextAlignVertical.center,
+                                      decoration: InputDecoration(
+                                          filled: true,
+                                          fillColor: Colors.white,
+                                          hintText: "Color",
+                                          hintStyle: TextStyle(color: Colors.grey,
+                                              fontWeight: FontWeight.bold),
+                                          border: OutlineInputBorder(
+                                              borderRadius: BorderRadius.circular(12),
+                                              borderSide: BorderSide.none
+                                          )
+                                      ),
+                                    ),
+                                  ],
                                 ),
                               ),
-                              SizedBox(height: 16,),
-                              TextFormField(
-
-                                textAlignVertical: TextAlignVertical.center,
-                                decoration: InputDecoration(
-                                    filled: true,
-                                    fillColor:Colors.white,
-                                    hintText: "Icon",hintStyle: TextStyle(color: Colors.grey, fontWeight: FontWeight.bold),
-                                    border: OutlineInputBorder(
-                                        borderRadius: BorderRadius.circular(12),
-                                        borderSide: BorderSide.none
-
-                                    )
-                                ),
-                              ),
-                              SizedBox(height: 16,),
-                              TextFormField(
-                                textAlignVertical: TextAlignVertical.center,
-                                decoration: InputDecoration(
-                                    filled: true,
-                                    fillColor:Colors.white,
-                                    hintText: "Color",hintStyle: TextStyle(color: Colors.grey, fontWeight: FontWeight.bold),
-                                    border: OutlineInputBorder(
-                                        borderRadius: BorderRadius.circular(12),
-                                        borderSide: BorderSide.none
-
-                                    )
-                                ),
-                              ),
-                            ],
-                          ),
+                            );
+                          }
                         );
-
-                          },
+                        },
                       );
                       },
                     ),
@@ -173,13 +278,14 @@ class _AddExpenseState extends State<AddExpense> {
                 width: double.infinity,
                 height: kToolbarHeight,
 
-                child: TextButton(onPressed: (){}, child: Text('Save', style: TextStyle(fontSize: 22, color:Colors.white),
-                ), style: TextButton.styleFrom(
+                child: TextButton(onPressed: (){},
+                  style: TextButton.styleFrom(
                   backgroundColor: Colors.black,
                   shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(12)
                   )
-                ) ,
+                ) , child: Text('Save', style: TextStyle(fontSize: 22, color:Colors.white),
+                ),
                 ),
               )
             ],
